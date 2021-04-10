ed0d66f07927d9ff91ca7a9a132c84547029ed19
abrabant <abrabant@student.42.fr>
2021-04-10 19:49:54 +0200

    Add assets for ft_read's module

--- src/module_ft_read.c
+++ src/module_ft_read.c
@@ -6,7 +6,7 @@
 /*   By: abrabant <abrabant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/10 15:09:48 by abrabant          #+#    #+#             */
-/*   Updated: 2021/04/10 18:12:10 by abrabant         ###   ########.fr       */
+/*   Updated: 2021/04/10 19:49:31 by abrabant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@
 #include <fcntl.h>
 #include <limits.h>
 #include <string.h>
+
 #include "smasher.h"
 #define READ_SIZE 100
 #define READ_BUFFER_SIZE 100000
@@ -28,18 +29,30 @@
 void	module_ft_read(t_module *module)
 {
 	const char	*filepaths[] = {
-		"asset/loremipsum1"
+		"asset/loremipsum1",
+		"asset/empty",
+		"asset/nonewline",
+		"asset/dir",
+		"asset/bigloremipsum",
+		"asset/idontactuallyexist",
+		"/etc/passwd",
 	};
 	char		expected[READ_BUFFER_SIZE] = { 0 };
 	char		actual[READ_BUFFER_SIZE] = { 0 };
+	char		*filename = NULL;
+	char		*filename_ptr = NULL;
 	int			actual_ret = -1;
 	int			expected_ret = -1;
 	int			ref_errno, actual_errno;
+	int			expected_read, actual_read;
 
 	for (size_t i = 0, length = sizeof (filepaths) / sizeof (char *); i < length; ++i) {
-		char	*filename = NULL;
-		char	*filename_ptr = NULL;
+		save_preserved_reg();
+		expected_read = actual_read = 0;
+		filename = NULL;
+		filename_ptr = NULL;
 
+		/* get the name of the file only */
 		if ((filename = strdup(filepaths[i])) != NULL) {
 			filename_ptr = filename;
 			filename = basename(filename);
@@ -51,12 +64,12 @@ void	module_ft_read(t_module *module)
 		
 		/* litteraly put the file's content in the expected buffer */
 		while ((expected_ret = read(fd, expected, READ_SIZE)) > 0)
-			;
+			expected_read += expected_ret;
 		ref_errno = errno;
 		lseek(fd, SEEK_SET, 0);
 		/* put the file's content in actual buffer */ 
 		while ((actual_ret = read(fd, actual, READ_SIZE)) > 0)
-			;
+			actual_read += actual_ret;
 		actual_errno = errno;
 
 		/* ensure errno is set correctly. If no error occured, errno will be
@@ -65,6 +78,10 @@ void	module_ft_read(t_module *module)
 		if (ref_errno != actual_errno)
 			smash(module, "ERRNO HAS NOT BEEN SET CORRECTLY!\n reference errno after call to read: %d\nactual errno"
 					"after call to ft_read: %d\n", ref_errno, actual_errno);
+		
+		/* compare return values, i.e number of bytes read */
+		if (actual_read != expected_read)
+			smash(module, "NUMBER OF BYTES READ DIFFER (return value)!\nThe read function has read %d\nYOUR ft_read has read %d", expected_read, actual_read);
 
 		/* check if read buffers are lexicographically the same */
 		if (strcmp(expected, actual) != 0)
@@ -91,7 +108,8 @@ void	module_ft_read(t_module *module)
 
 			smash(module, "READ BUFFERS DIFFER!\nDifference has been outputed in log/%s", logfile);
 		}
-		else if (smash_assert_reg(module) && ref_errno == actual_errno)
+		/* if everything is okay and registers have been preserved, the test passes. */
+		else if (smash_assert_reg(module) && ref_errno == actual_errno && expected_read == actual_read)
 			module->passed++;
 		module->tested++;
 		free(filename_ptr);
