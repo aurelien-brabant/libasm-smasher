f588767b50079d65fcab6a59acc1ac37451242e3
abrabant <abrabant@student.42.fr>
2021-04-24 20:08:31 +0200

    Improve Makefile

--- init.sh
+++ init.sh
@@ -3,5 +3,8 @@
 # If BONUS environment variable is set, compile smasher with support for bonuses. BONUS=0 by default.
 # NOTE: in all cases, the resulting archive must be EXACTLY called libasm.a. Otherwise the smasher will not compile.
 
-[ $BONUS -eq 1 ] && make rebonus LIBASM_PATH=/libasm || make re LIBASM_PATH=/libasm
+[ $BONUS -eq  1 ] && suffix=bonus || suffix= 
+
+make re$suffix LIBASM_PATH=/libasm
 ./smasher*
+make fclean$suffix LIBASM_PATH=/libasm
