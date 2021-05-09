6c7dd6c63b6f7011c5dbf0489f3945ae2ac2ab6d
abrabant <abrabant@student.42.fr>
2021-05-09 23:07:09 +0200

    Add more tests for atoi_base

--- src/module_ft_atoi_base.c
+++ src/module_ft_atoi_base.c
@@ -15,10 +15,26 @@ void	module_ft_atoi_base(t_module *module)
 		// INT_MAX in octal
 		{ "17777777777", "01234567" },
 		// INT_MIN in binary
-		{ "\v\r   \t\v-+-+-++++++++10000000000000000000000000000000", "01" }
+		{ "\v\r   \t\v-+-+-++++++++10000000000000000000000000000000", "01" },
+		{ "0", "01" },
+		{ "p", "poneyvif" },
+		{ "o", "poneyvif" },
+		/* INCORRECT BASES */
+		{ "123", "01234567889" }, /* duplicated */
+		{ "12", "0123456 789" }, /* base with space */
+		{ "12", "0123456\v789" }, /* base with space */
+		{ "12", "0123456\f789" }, /* base with space */
+		{ "12", "0123456\t789" }, /* base with space */
+		{ "12", "0123456\r789" }, /* base with space */
+		{ "12", "0123456+789" }, /* base with space */
+		{ "12", "0123456-789" }, /* base with sign */
+		/* Stop at first non-base char */
+		{ "-12a3", "0123456789" },
+		{ "\v\t\t\t\t\t\r \v\v\v \t\t\t\t   a1234", "0123456789" }, 
 	};
+
 	int	expected[] = {
-		123, 15, 15, 255, 7, 128, 0, 0, INT_MAX, INT_MIN
+		123, 15, 15, 255, 7, 128, 0, 0, INT_MAX, INT_MIN, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, -12, 0
 	};
 	size_t	size = sizeof (args) / (sizeof (char *) * 2);
 
